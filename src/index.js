d95615583cccd4120b8909bf4198302017918480
DavidFuents <Davidf0827@gmail.com>
2020-11-11 19:58:47 -0500

    commit tree ca02a8e1cd23835ac4a8379458211708ebcda6df

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+document.addEventListener("DOMContentLoaded", function{
+    fetch(imgUrl)
+        .then(response => response.json())
+        .then(json => return json);
+});
